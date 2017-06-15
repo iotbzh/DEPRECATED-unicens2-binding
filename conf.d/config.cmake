bbe0fa2bb0479bf7a09eed345fefc3c375773745
Sebastien Douheret <sebastien.douheret@iot.bzh>
2017-06-15 15:45:17 +0200

    Use latest version of conf.d/templates submodule.
    Changes of conf.d/templates:
    
    (8d9b7f2 - Romain Forlot) Merge changes I81a6d9f6,I67698bbb
    (b0a2632 - Romain Forlot) Fix: remove old bug about overwriten variables.
    (5a83e47 - Romain Forlot) Update README
    (abd65a3 - Sebastien Douheret) Fix gdb debugging and add widget install on target
    (7315692 - Romain Forlot) Update README
    (d8fcd75 - Romain Forlot) Merge "Update config.cmake.sample"
    (4dba246 - Romain Forlot) Merge "Replace LIBRARIES by LDFLAGS for pkgconfig dep"
    (21ae95d - Romain Forlot) Fix:WIDGET_ENTRY_POINT variable always overwritten
    (4809e42 - Romain Forlot) renamed default submodules dir
    (2bb223e - Romain Forlot) Fix wrong path to submodule files.
    (259e135 - Romain Forlot) Format
    (af3cc64 - Ronan Le Martret) Replace LIBRARIES by LDFLAGS for pkgconfig dep
    (695291d - Ronan Le Martret) Update config.cmake.sample
    (c1fd793 - Jan-Simon Moeller) Merge "Set default value for APP_TEMPLATES_DIR
                                  and WIDGET_ENTRY_POINT"
    (f3e177c - Stephane Desneux) gitignore: add vim swap files
    (7e6516c - Stephane Desneux) fix typo: 'license', not 'licence' (french)
    
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- conf.d/config.cmake
+++ conf.d/config.cmake
@@ -33,9 +33,25 @@ set(PROJECT_LANGUAGES,"C")
 # relative to the root project directory
 set(PROJECT_APP_TEMPLATES_DIR "conf.d/templates")
 
+# Where are stored your external libraries for your project. This is 3rd party library that you don't maintain
+# but used and must be built and linked.
+# set(PROJECT_LIBDIR "libs")
+
+# Where are stored data for your application. Pictures, static resources must be placed in that folder.
+# set(PROJECT_RESOURCES "data")
+
+# Which directories inspect to find CMakeLists.txt target files
+set(PROJECT_SRC_DIR_PATTERN "[^_]*")
+
 # Compilation Mode (DEBUG, RELEASE)
 # ----------------------------------
 set(CMAKE_BUILD_TYPE "DEBUG")
+
+# Kernel selection if needed. Impose a minimal version.
+# NOTE FOR NOW IT CHECKS KERNEL Yocto SDK Kernel version
+# else only HOST VERSION
+# ------------------------------------------------------
+#set (kernel_minimal_version 4.8)
 
 # Compiler selection if needed. Overload the detected compiler.
 # -----------------------------------------------
@@ -72,47 +88,65 @@ set(INSTALL_PREFIX $ENV{HOME}/opt)
 set(CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX}/lib64/pkgconfig ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig)
 set(LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib)
 
+# Optional location for config.xml.in
+# -----------------------------------
+#set(WIDGET_CONFIG_TEMPLATE ${CMAKE_CURRENT_SOURCE_DIR}/conf.d/config.xml.in)
+
+# Mandatory widget Mimetype specification of the main unit
+# --------------------------------------------------------------------------
+# Choose between :
+#- text/html : HTML application,
+#	content.src designates the home page of the application
+#
+#- application/vnd.agl.native : AGL compatible native,
+#	content.src designates the relative path of the binary.
+#
+# - application/vnd.agl.service: AGL service, content.src is not used.
+#
+#- ***application/x-executable***: Native application,
+#	content.src designates the relative path of the binary.
+#	For such application, only security setup is made.
+#
+set(WIDGET_TYPE application/vnd.agl.service)
+
+# Mandatory Widget entry point file of the main unit
+# --------------------------------------------------------------
+# This is the file that will be executed, loaded,
+# at launch time by the application framework.
+#
+set(WIDGET_ENTRY_POINT lib/afb-ucs2.so)
+
 # Optional dependencies order
 # ---------------------------
 #set(EXTRA_DEPENDENCIES_ORDER)
 
 # Optional Extra global include path
 # -----------------------------------
-# set(EXTRA_INCLUDE_DIRS)
+#set(EXTRA_INCLUDE_DIRS)
 
 # Optional extra libraries
 # -------------------------
-# set(EXTRA_LINK_LIBRARIES)
+#set(EXTRA_LINK_LIBRARIES)
 
 # Optional force binding installation
 # ------------------------------------
 # set(BINDINGS_INSTALL_PREFIX PrefixPath )
 
-# Optional force widget prefix generation
-# ------------------------------------------------
-# set(WIDGET_PREFIX DestinationPath)
-
-# Optional Widget entry point file.
-# ---------------------------------------------------------
-# This is the file that will be executed, loaded,...
-# at launch time by the application framework
-
-# set(WIDGET_ENTRY_POINT EntryPoint_Path)
-
-# Optional Widget Mimetype specification
-# --------------------------------------------------
-# Choose between :
-# - application/x-executable
-# - application/vnd.agl.url
-# - application/vnd.agl.service
-# - application/vnd.agl.native
-# - text/vnd.qt.qml
-# - application/vnd.agl.qml
-# - application/vnd.agl.qml.hybrid
-# - application/vnd.agl.html.hybrid
-#
-set(WIDGET_TYPE application/vnd.agl.service)
-
 # Optional force binding Linking flag
 # ------------------------------------
 # set(BINDINGS_LINK_FLAG LinkOptions )
+
+# Optional force package prefix generation, like widget
+# -----------------------------------------------------
+# set(PKG_PREFIX DestinationPath)
+
+# Optional Application Framework security token
+# and port use for remote debugging.
+#------------------------------------------------------------
+#set(AFB_TOKEN   ""      CACHE PATH "Default AFB_TOKEN")
+#set(AFB_REMPORT "1234" CACHE PATH "Default AFB_TOKEN")
+
+# This include is mandatory and MUST happens at the end
+# of this file, else you expose you to unexpected behavior
+# -----------------------------------------------------------
+include(${PROJECT_APP_TEMPLATES_DIR}/cmake/common.cmake)
